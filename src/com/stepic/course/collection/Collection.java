afa04f65bb70a1b8dff860b777fe143285673c46
Yurii Krynytskyi <yuri.krynytsky@gmail.com>
2020-07-29 18:28:30 +0300

    collection method toArray()

---
+++ src/com/stepic/course/collection/Collection.java
@@ -0,0 +1,41 @@
+package com.stepic.course.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Collection {
+    public static void main(String[] args) {
+
+        List<GreekLetter> letterList = new ArrayList<>();
+        letterList.add(new GreekLetter("Gamma",  3));
+        letterList.add(new GreekLetter("Omega", 24));
+        letterList.add(new GreekLetter("Alpha",  1));
+
+// collection method toArray()
+        Object[] objArray = letterList.toArray();
+
+
+        for (int i = 0; i < objArray.length; i++) {
+            System.out.println(objArray[i]);
+        }
+    }
+
+    static class GreekLetter {
+
+        private String letter;
+        private Integer position;
+
+        public GreekLetter(String letter, Integer position) {
+            this.letter = letter;
+            this.position = position;
+        }
+
+        @Override
+        public String toString() {
+            return "{" +
+                    "letter='" + letter + '\'' +
+                    ", position=" + position +
+                    '}';
+        }
+    }
+}
