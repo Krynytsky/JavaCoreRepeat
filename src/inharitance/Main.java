250e38e6fe871c49b3886024a8fe1aa2b5a5f719
Yurii Krynytskyi <yuri.krynytsky@gmail.com>
2020-08-19 01:12:42 +0300

    Composition and agregation

--- src/inharitance/Main.java
+++ src/inharitance/Main.java
@@ -9,16 +9,12 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        Address address1 = new Address("Lviv", "Pryrodna", 79000);
-        Address address2 = new Address("Sambir", "Ivasjyka", 81400);
-        Address address3 =new Address("Lviv", "Vugovskogo", 79000);
-        Address address4 = new Address("Drogobych", "Bichna", 44298);
 
         List<Employer> employerList = new ArrayList<>();
-        employerList.add(new Employer(1,"Yurii",address1));
-        employerList.add(new Employer(2,"Oksana",address2));
-        employerList.add(new Employer(3,"Petro",address3));
-        employerList.add(new Employer(4,"Vika",address4));
+        employerList.add(new Employer(1,"Yurii","Lviv","prurodnja",81400));
+        employerList.add(new Employer(2,"Oksana","Sambir", "Ivasjyka", 81400));
+        employerList.add(new Employer(3,"Petro","Lviv", "Vugovskogo", 79000));
+        employerList.add(new Employer(4,"Vika","Drogobych", "Bichna", 44298));
 
         for (Employer employer:employerList){
             System.out.println(employer);
